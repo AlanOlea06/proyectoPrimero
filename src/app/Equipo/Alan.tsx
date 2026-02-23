1c00e29d0e46946df83aeed41c52a5b554c82d5f
AlanOlea06 <alanoleabatres@gmail.com>
2026-02-23 07:25:00 -0600

    Refactor Persona component to use external styles and implement dynamic data structure for team members

--- src/app/Equipo/Alan.tsx
+++ src/app/Equipo/Alan.tsx
@@ -1,14 +1,17 @@
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { Image, Text, View } from "react-native";
+import { Styles } from "../EquipoStyles/Alan";
 
 export const Persona = () => {
     const params = useLocalSearchParams();
+
+
     console.log(params);
     return ( 
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>{params.nombre}</Text>
-            <Image source={require(`../../assets/image/alan.jpeg`)} style={{width: 100, height: 100}} />
+        <View style={Styles.content}>
+            <Text style={Styles.nombre}>{params.nombre}</Text>
+            <Image source={require('../../assets/image/alan.jpeg')} style={Styles.imagen} />
             <Text>{params.descripcion}</Text>
         </View>
     )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,21 @@
 import { View } from "react-native";
+import { diccionarioEquipo } from "../components/shared/datosEquipo";
 import { Tarjeta } from "../components/Tarjeta";
 import { Styles } from "../styles/Styles";
 
 export default function Index() {
-  // 1. Transformamos el arreglo de strings a un arreglo de objetos
   // Aquí es donde Metro Bundler verá los require() y empaquetará las imágenes
-  const tarjetas = [
-    { nombre: 'Alan', imagen: require('../assets/image/alan.jpeg') },
-    { nombre: 'Ivan', imagen: require('../assets/image/prueba.png') },
-    { nombre: 'Diego', imagen: require('../assets/image/prueba.png') },
-    { nombre: 'Tony', imagen: require('../assets/image/prueba.png') },
-    { nombre: 'Paul', imagen: require('../assets/image/prueba.png') }
-  ];
+  const tarjetas = Object.values(diccionarioEquipo);
 
   return (
     <View style={Styles.inicio}>
-      {/* 2. Ahora iteramos sobre los objetos ("item") en lugar de solo strings */}
-      {tarjetas.map((item, index) => (
+      {/* 2. Ahora iteramos sobre los objetos ("persona") en lugar de solo strings */}
+      {tarjetas.map((persona, index) => (
         <Tarjeta 
           key={index} 
-          nombre={item.nombre} // Sacamos el nombre del objeto
-          descripcion={`Esta es la descripción de la tarjeta ${index + 1}`} 
-          imagen={item.imagen} // Le pasamos la imagen ya requerida a tu componente
+          nombre={persona.nombre} // Sacamos el nombre del objeto
+          descripcion={persona.descripcion} 
+          imagen={persona.imagen} // Le pasamos la imagen ya requerida a tu componente
         />
       ))}
     </View>
