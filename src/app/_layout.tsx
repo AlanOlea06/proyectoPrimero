b6070b8f508ea23e6580aa814877361230626074
DL-0612 <diego104g3@gmail.com>
2026-02-25 07:36:49 -0600

    FONT corregida

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,7 +7,7 @@ import FinbalanceLogo from "../components/FinbalanceLogo";
 export default function RootLayout() {
   const [loaded] = useFonts({ 
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    OdorMeanChey: require("../assets/fonts/OdorMeanChey-Regular.ttf")
+    OdorMeanChey: require("../assets/fonts/odor-mean-chey-v31-latin-regular.ttf")
     // Add the Odor Mean Chey font file to `src/assets/fonts` and uncomment the line below
     // OdorMeanChey: require("../assets/fonts/OdorMeanChey-Regular.ttf")
   });
@@ -21,8 +21,8 @@ export default function RootLayout() {
       <StatusBar style="auto" />
       <Stack 
         screenOptions={{ 
-          headerTitleStyle: { fontFamily: "Odor Mean Chey, serif" },
-          headerBackTitleStyle: { fontFamily: "Odor Mean Chey, serif" }
+          headerTitleStyle: { fontFamily: "OdorMeanChey" },
+          headerBackTitleStyle: { fontFamily: "OdorMeanChey" }
         }}
       >
         <Stack.Screen 
--- src/styles/Styles.ts
+++ src/styles/Styles.ts
@@ -22,7 +22,7 @@ export const Styles = StyleSheet.create({
         fontSize: 35,
         fontWeight: 'bold',
         color: 'black',
-        fontFamily: 'Odor Mean Chey, serif',
+        fontFamily: 'OdorMeanChey',
     },
 
     topBarTarjeta: {
