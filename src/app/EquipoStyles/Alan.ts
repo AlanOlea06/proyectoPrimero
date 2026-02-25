f616f81c8c4b8e5cba23831460909da4d0472f6a
AlanOlea06 <alanoleabatres@gmail.com>
2026-02-25 12:30:20 -0600

    Refactor Alan component layout: encapsulate text elements in a dedicated view and enhance styling for better readability

--- src/app/EquipoStyles/Alan.ts
+++ src/app/EquipoStyles/Alan.ts
@@ -17,13 +17,32 @@ export const Styles = StyleSheet.create({
         fontSize: 16,
     },
     habilidades: {
-        backgroundColor: 'red',
-
+        color: '#0b9387',
+        backgroundColor: '#3a888362',
+        borderRadius: 5,
+        fontWeight: 'bold',
+        overflow: 'hidden',
+        alignSelf: 'flex-start', // Hace que el ancho sea solo el del contenido
+        paddingHorizontal: 10,   // Da espacio a los lados del texto
+        paddingVertical: 2,      // Da espacio arriba y abajo
+        //marginBottom: 5,
     },
     constant: {
         marginBottom: 20,
         width: '100%',
-        backgroundColor: '#0b9387'
+        backgroundColor: '#0b9387',
+        flexDirection: 'row',
+        color: '#ffffff',
+        fontSize: 18,
+    },
+    textConstant: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignSelf: 'center',
+        paddingLeft: 10,
+        width: '80%',
+        height: '80%',
+
     }
 });
 
