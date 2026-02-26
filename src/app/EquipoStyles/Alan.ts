8dce683a77a48c3233c680d30860d469d7e1f94c
AlanOlea06 <alanoleabatres@gmail.com>
2026-02-25 22:03:29 -0600

    Add Terminal component and enhance Alan profile with interactive features

--- src/app/EquipoStyles/Alan.ts
+++ src/app/EquipoStyles/Alan.ts
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native';
 export const Styles = StyleSheet.create({
     content: {
         flex: 1,
+        paddingTop: 0,
         padding: 20,
     },
     imagen: {
@@ -32,8 +33,15 @@ export const Styles = StyleSheet.create({
         marginTop: 10,
         fontSize: 16,
     },
+    contenedorParrafo: {
+        width: '100%',
+        backgroundColor: '#3a888362',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10 
+    },
     constant: {
-        marginBottom: 20,
+        marginBottom: 5,
         width: '100%',
         backgroundColor: '#0b9387',
         flexDirection: 'row',
@@ -53,7 +61,35 @@ export const Styles = StyleSheet.create({
         color: '#ffffff',
         fontSize: 18,
         fontWeight: 'bold',
-    }
+    },
+
+    button: {
+  backgroundColor: '#0b9387',  // or whatever color you use
+  paddingVertical: 10,
+  paddingHorizontal: 16,
+  borderRadius: 8,
+  marginBottom: 10,
+},
+buttonText: {
+  color: '#fff',
+  fontSize: 14,
+  fontWeight: '600',
+  flexShrink: 1,           // key: lets text shrink instead of overflow
+    textAlign: 'center',    // center text if it wraps to multiple lines
+},
+
+
 });
 
 export default Styles;
+{/*button: {
+        backgroundColor: '#0b9387',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+        height: 10,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },*/}
